2392e86644c99f2e09c60c80bf3dca772db47cb9
Nam Hoang Nguyen <namqhong@gmail.com>
2019-05-03 22:48:45 +0700

    them unit test cho app

---
+++ FBToken.Tests/FacebookTokenServiceTest.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Dynamic;
+using System.IO;
+using System.Threading.Tasks;
+using FBToken.Main.Core;
+using FBToken.Main.Models;
+using Moq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace FBToken.Tests
+{
+    [TestFixture]
+    public class FacebookTokenServiceTest
+    {
+        [Test]
+        public async Task FacebookTokenService_RightUsernameAndPassword_ReturnToken()
+        {
+            //Arrange
+            string email = "testemail@gmail.com";
+            string password = "passoftest";
+
+            string baseRequestUrl = @"https://b-graph.facebook.com/auth/login";
+            string requestParamString =
+                $"email={email}&password={password}&access_token=6628568379|c1e620fa708a1d5696fb991c1bde5662&method=post";
+
+            string token = "token_123456789";
+
+            FBGetTokenRequesterStub_Success requesterStub = new FBGetTokenRequesterStub_Success("GetTokenResponse_DataFiles\\GetTokenResponse_Content_Success.json");
+            FacebookTokenService tokenServiceSUT = new FacebookTokenService(requesterStub);
+
+            //Act
+            UserTokenInfo tokenInfoResult = await tokenServiceSUT.GetTokenInfoAsync(email, password);
+
+            //Assert
+            Assert.AreEqual($"{baseRequestUrl}?{requestParamString}", requesterStub.CalledUrl);
+            Assert.AreEqual(token, tokenInfoResult.AccessToken);
+        }
+
+        [Test]
+        public async Task FacebookTokenService_RightUsernameAndPassword_CheckpointException()
+        {
+            //Arrange
+            string email = "testemail@gmail.com";
+            string password = "passoftest";
+
+            string baseRequestUrl = @"https://b-graph.facebook.com/auth/login";
+            string requestParamString =
+                $"email={email}&password={password}&access_token=6628568379|c1e620fa708a1d5696fb991c1bde5662&method=post";
+
+            FBGetTokenRequesterStub_Checkpoint requesterStub = new FBGetTokenRequesterStub_Checkpoint();
+            FacebookTokenService tokenServiceSUT = new FacebookTokenService(requesterStub);
+
+            Exception threwException = null;
+
+            //Act
+            try
+            {
+                UserTokenInfo tokenInfoResult = await tokenServiceSUT.GetTokenInfoAsync(email, password);
+            }
+            catch (Exception ex)
+            {
+                threwException = ex;
+            }
+
+            //Assert
+            Assert.IsInstanceOf<FacebookUserCheckPointException>(threwException);
+        }
+    }
+
+    public class FBGetTokenRequesterStub_Success : IWebRequester
+    {
+        //https://stackoverflow.com/questions/5311023/mocking-generic-method-call-for-any-given-type-parameter
+
+        private string _responseDataFilePath;
+
+        public FBGetTokenRequesterStub_Success(string responseResponseDataFilePath)
+        {
+            _responseDataFilePath = responseResponseDataFilePath;
+        }
+
+        public Task PostRequestAsync<T>(string endpoint, object data, string args = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<T> GetRequestAsync<T>(string endpoint, string args = null)
+        {
+            CalledUrl = $"{endpoint}?{args}";
+
+            //https://stackoverflow.com/questions/6080596/how-can-i-load-this-file-into-an-nunit-test
+            string praticeResponseResult = File.ReadAllText(Path.Combine(TestContext.CurrentContext.TestDirectory, _responseDataFilePath));
+            T responseResult = JsonConvert.DeserializeObject<T>(praticeResponseResult);
+            return Task.FromResult(responseResult);
+        }
+
+        public string CalledUrl { get; set; }
+    }
+
+    public class FBGetTokenRequesterStub_Checkpoint : IWebRequester
+    {
+        public Task PostRequestAsync<T>(string endpoint, object data, string args = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetRequestAsync<T>(string endpoint, string args = null)
+        {
+            throw new FacebookUserCheckPointException();
+        }
+    }
+}
---
+++ FBToken.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FBToken.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FBToken.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b843c4b8-dba3-49a8-8dfd-f951a98b5f29")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
